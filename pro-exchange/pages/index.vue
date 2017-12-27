8cd047ccafb25ca7974198bf67b78cb729ff269c
Evald24 <evaldsmalyakov@mail.ru>
2017-12-28 02:02:32 +0600

    Fix

--- pro-exchange/pages/index.vue
+++ pro-exchange/pages/index.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="Content">
       <Exchange />
-      <Authorization ref="ModalAuthorization" />
     </div>
 </template>
 
 <script>
 import Exchange from '~/components/Exchange'
-import Authorization from '~/components/Modals/Authorization'
 
 export default {
   head () {
